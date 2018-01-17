3e07722968e5329783437d3b3200760e940615cf
guygu7 <599266988@qq.com>
2018-01-17 16:08:34 +0800

    1

--- test.js
+++ test.js
@@ -146,9 +146,9 @@ LC.Utils.Map = function() {
 					}
 				}
 			} catch (e) {
-				return false;
+				return null;
 			}
-			return false;
+			return null;
 		};
 	};
 	//获取指定索引的元素（使用element.key，element.value获取KEY和VALUE），失败返回NULL
@@ -276,7 +276,7 @@ LC.Utils.Map = function() {
  * 公共属性字典
  */
 LC.CommonProperty = {
-	sign : "lcsign" //通用标记属性，替代id
+	SIGN : "lcsign" //通用标记属性，替代id
 };
 /**
  * 定义组件命名空间
@@ -322,7 +322,7 @@ LC.Components.ComponentFunction = {
 						param = ["1"];
 						//校验到错误则设为1，提高容错率
 					} else {
-						param = args[0];
+						param = args;
 						//校验通过，赋值
 					}
 				} else {
@@ -366,7 +366,7 @@ LC.Components.ComponentFunction = {
 			if (args[0] instanceof Array) {//是数组
 				for (var i = 0; i < args[0].length; i++) {//遍历数组校验是否字符串或正整数
 					if ( typeof args[0][i] == 'string' || typeof args[0][i] == 'number') {//类型校验通过则进行正整数校验
-						if (!new RegExp(/^[0-9]+\.?[0-9]*$/).test(args[0][i]))
+						if (!new RegExp(/^[0-9]+(\.\d)?[0-9]*$/).test(args[0][i]))
 							LC.warning(msg + "参数错误,必须是正浮点数。");
 					} else {
 						LC.warning(msg + "参数错误,必须是正浮点数。");
@@ -376,7 +376,7 @@ LC.Components.ComponentFunction = {
 				//是数组则直接复制
 			} else {//不是数组
 				if ( typeof args[0] == 'string' || typeof args[0] == 'number') {//类型校验通过则进行正整数校验
-					if (!new RegExp(/^[0-9]+\.?[0-9]*$/).test(args[0])) {
+					if (!new RegExp(/^[0-9]+(\.\d)?[0-9]*$/).test(args[0])) {
 						LC.warning(msg + "参数错误,必须是正浮点数。");
 						param = ["0"];
 						//校验到错误则设为0，提高容错率
@@ -393,7 +393,7 @@ LC.Components.ComponentFunction = {
 		} else {//传多个参数
 			for (var i = 0; i < args.length; i++) {
 				if ( typeof args[i] == 'string' || typeof args[i] == 'number') {//类型校验通过则进行正整数校验
-					if (!new RegExp(/^[0-9]+\.?[0-9]*$/).test(args[i]))
+					if (!new RegExp(/^[0-9]+(\.\d)?[0-9]*$/).test(args[i]))
 						LC.warning(msg + "参数错误,必须是正浮点数。");
 				} else {
 					LC.warning(msg + "参数错误,必须是正浮点数。");
@@ -402,21 +402,67 @@ LC.Components.ComponentFunction = {
 			param = args;
 		}
 		return param;
+	},
+	/**
+	 * 传参校验，校验传入的参数是否为正整数，有错则报警并设为默认值1，但程序不会终止
+	 * @param {Object} num 需要校验的参数(非必须，未传则设为默认值1)
+	 * @param {Object} msg 报警提示信息
+	 * @return param[]
+	 */
+	checkNumInt : function(num, msg) {
+		if (null != num) {
+			if ( typeof num == 'string' || typeof num == 'number') {//类型校验通过则进行正整数校验
+				if (!new RegExp(/^[1-9]\d*$/).test(num)) {
+					LC.warning(msg + "参数错误,必须是正整数。");
+					num = 1;
+					//校验到错误则设为1，提高容错率
+				} else {//校验通过
+				};
+			} else {
+				LC.warning(msg + "参数错误,必须是正整数。");
+				num = 1;
+				//校验到错误则设为1，提高容错率
+			};
+		} else {//未传参数默认第一个
+			num = 1;
+		}
+		return num;
 	}
 };
 /**
- * 组件空间中装入进度条类
+ * 组件空间中装入进度条类,支持方法：
+ * getSignID() 获取进度条自定义ID
+ * setSignID(ID) 设置进度条自定义ID
+ * getRunningTime() 获取进度条过渡效果运行时间Map
+ * setRunningTime(time, num) 设置进度条过渡效果运行时间，单位s（只能设置一个子进度条）
+ * getWidth() 获取进度条要运行到的百分比Map
+ * setWidth(width, num) 设置进度条要运行到的百分比（只能设置一个子进度条）
+ * addStriped(args) 按传入顺位号添加条纹
+ * removeStriped(args) 按传入顺位号移除条纹
+ * addActive(args) 按传入顺位号添加条纹动画
+ * removeActive(args) 按传入顺位号移除条纹动画
+ * styleAlter(progress) 进度条样式修改(子类继承重写使用)
+ * creatProgressDOM(num) 创建进度条的DOM对像,传入需要创建的子进度条的个数
+ * addListener(fn, num) 设置监听，在读条完成后调用函数（只能设置一个子进度条）
+ * removeListener(fn, num) 移除监听，移除在读条完成后调用函数（只能设置一个子进度条）
+ * setTransition(str, num) 修改过渡效果(一次只能设置一个，除长度外，可以在尾部添加其他过渡效果)
+ * getTransition() 获取进度条过渡效果Map
+ * removeTransition(str, num) 还原过渡效果(一次只能设置一个)
+ * start()  运行，开始读条到设定百分比
+ *
+ * progressDOM 进度条的DOM对像，通过.append()加入页面
+ *
  * @param {Object} progressID 进度条id
  */
 LC.Components.ProgressBar = function(progressID) {
-	var signID = progressID;
+	var _signID = progressID;
 	//进度条自定义ID
 	if ( typeof this.getSignID != 'function') {
 		/**
 		 * 获取进度条自定义ID
 		 */
 		LC.Components.ProgressBar.prototype.getSignID = function() {
-			return signID;
+			return _signID;
 		};
 	};
 	if ( typeof this.setSignID != 'function') {
@@ -424,18 +470,18 @@ LC.Components.ProgressBar = function(progressID) {
 		 * 设置进度条自定义ID
 		 */
 		LC.Components.ProgressBar.prototype.setSignID = function(ID) {
-			signID = ID;
+			_signID = ID;
 			return this;
 		};
 	};
-	var runningTime = new LC.Utils.Map();
+	var _runningTimeMap = new LC.Utils.Map();
 	//进度条过渡效果运行时间
 	if ( typeof this.getRunningTime != 'function') {
 		/**
 		 * 获取进度条过渡效果运行时间map
 		 */
 		LC.Components.ProgressBar.prototype.getRunningTime = function() {
-			return runningTime;
+			return _runningTimeMap;
 		};
 	};
 	if ( typeof this.setRunningTime != 'function') {
@@ -446,43 +492,29 @@ LC.Components.ProgressBar = function(progressID) {
 		 * @return this
 		 */
 		LC.Components.ProgressBar.prototype.setRunningTime = function(time, num) {
-			if (null!=time){
+			if (null != time) {
 				if ( typeof time == 'string' || typeof time == 'number') {//类型校验通过则进行正浮点数数校验
-					if (!new RegExp(/^[0-9]+\.?[0-9]*$/).test(time)) {
-						LC.warning("参数错误,必须是正浮点数。");
+					if (!new RegExp(/^[0-9]+(\.\d)?[0-9]*(s?|S?)$/).test(time)) {
+						LC.warning("设置进度条过渡效果运行时长方法setRunningTime参数错误,必须是正浮点数。");
 						time = 0;
 						//校验到错误则设为0，提高容错率
-					} else {//校验通过
+					} else {//校验通过,如果尾部有"S"，则去掉
+						time = time.toString().replace("s", "").replace("S", "");
 					};
 				} else {
-					LC.warning("参数错误,必须是正浮点数。");
+					LC.warning("设置进度条过渡效果运行时长方法setRunningTime参数错误,必须是正浮点数。");
 					time = 0;
 					//校验到错误则设为0，提高容错率
 				};
 			} else {//未传参数默认0S
 				time = 0;
 			}
-			if (null!=num){
-				if ( typeof num == 'string' || typeof num == 'number') {//类型校验通过则进行正整数校验
-					if (!new RegExp(/^[1-9]\d*$/).test(num)) {
-						LC.warning("参数错误,必须是正整数。");
-						num = 1;
-						//校验到错误则设为1，提高容错率
-					} else {//校验通过
-					};
-				} else {
-					LC.warning("参数错误,必须是正整数。");
-					num = 1;
-					//校验到错误则设为1，提高容错率
-				};
-			} else {//未传参数默认第一个
-				num = 1;
-			}
-			runningTime.put(num, time);
+			num = LC.Components.ComponentFunction.checkNumInt(num, "设置进度条过渡效果运行时长方法setRunningTime");
+			_runningTimeMap.put(num.toString(), time.toString());
 			return this;
 		};
 	};
-	var width = new LC.Utils.Map();
+	var _widthMap = new LC.Utils.Map();
 	//进度条长度（百分比）
 	if ( typeof this.setWidth != 'function') {
 		/**
@@ -490,24 +522,42 @@ LC.Components.ProgressBar = function(progressID) {
 		 * @param {Object} width 百分比
 		 * @return this
 		 */
-		LC.Components.ProgressBar.prototype.setWidth = function(width,num) {
-			width = width;
+		LC.Components.ProgressBar.prototype.setWidth = function(width, num) {
+			if (null != width) {
+				if ( typeof width == 'string' || typeof width == 'number') {//类型校验通过则进行正浮点数数校验
+					if (!new RegExp(/^[0-9]+(\.\d)?[0-9]*\%?$/).test(width)) {
+						LC.warning("设置进度条百分比方法setWidth参数错误,必须是正浮点数。");
+						width = 0;
+						//校验到错误则设为0%，提高容错率
+					} else {//校验通过,如果尾部有"%"，则去掉
+						width = width.toString().replace("%", "");
+					};
+				} else {
+					LC.warning("设置进度条百分比方法setWidth参数错误,必须是正浮点数。");
+					width = 0;
+					//校验到错误则设为0%，提高容错率
+				};
+			} else {//未传参数默认0%
+				width = 0;
+			}
+			num = LC.Components.ComponentFunction.checkNumInt(num, "设置进度条百分比方法setWidth");
+			_widthMap.put(num.toString(), width.toString());
 			return this;
 		};
 	};
 	if ( typeof this.getWidth != 'function') {
 		/**
-		 * 获取进度条要运行到的百分比
-		 * @return this
+		 * 获取进度条要运行到的百分比Map
+		 * @return _widthMap
 		 */
 		LC.Components.ProgressBar.prototype.getWidth = function() {
-			return width;
+			return _widthMap;
 		};
 	};
 	//添加条纹效果
 	if ( typeof this.addStriped != 'function') {
 		/**
-		 * 传入需要添加条纹的顺位
+		 * 按传入顺位号添加条纹
 		 * @param args args[] ...args
 		 * @return this
 		 */
@@ -524,7 +574,7 @@ LC.Components.ProgressBar = function(progressID) {
 						});
 					};
 				} else {
-					LC.warning("未获取到有效的子进度条。");
+					LC.warning("添加条纹效果方法addStriped未获取到有效的子进度条。");
 				}
 			};
 			return this;
@@ -533,7 +583,7 @@ LC.Components.ProgressBar = function(progressID) {
 	//去除条纹效果
 	if ( typeof this.removeStriped != 'function') {
 		/**
-		 * 传入需要去除条纹的顺位
+		 *  按传入顺位号移除条纹
 		 * @param args args[] ...args
 		 * @return this
 		 */
@@ -550,7 +600,7 @@ LC.Components.ProgressBar = function(progressID) {
 						});
 					};
 				} else {
-					LC.warning("未获取到有效的子进度条。");
+					LC.warning("移除条纹效果方法removeStriped未获取到有效的子进度条。");
 				}
 			};
 			return this;
@@ -559,7 +609,7 @@ LC.Components.ProgressBar = function(progressID) {
 	//添加条纹动画效果
 	if ( typeof this.addActive != 'function') {
 		/**
-		 * 传入需要添加条纹动画的顺位
+		 * 按传入顺位号添加条纹动画
 		 * @param args args[] ...args
 		 * @return this
 		 */
@@ -576,7 +626,7 @@ LC.Components.ProgressBar = function(progressID) {
 						});
 					};
 				} else {
-					LC.warning("未获取到有效的子进度条。");
+					LC.warning("添加条纹动画效果方法addActive未获取到有效的子进度条。");
 				}
 			};
 			return this;
@@ -585,7 +635,7 @@ LC.Components.ProgressBar = function(progressID) {
 	//去除条纹动画效果
 	if ( typeof this.removeActive != 'function') {
 		/**
-		 * 传入需要去除条纹动画的顺位
+		 * 按传入顺位号移除条纹动画
 		 * @param args args[] ...args
 		 * @return this
 		 */
@@ -602,7 +652,7 @@ LC.Components.ProgressBar = function(progressID) {
 						});
 					};
 				} else {
-					LC.warning("未获取到有效的子进度条。");
+					LC.warning("移除条纹动画效果方法removeActive未获取到有效的子进度条。");
 				}
 			};
 			return this;
@@ -627,9 +677,9 @@ LC.Components.ProgressBar = function(progressID) {
 				num = 1;
 			};
 			var param = LC.Components.ComponentFunction.checkInt([num], "创建进度条方法creatProgressDOM");
-			var sign = LC.CommonProperty.sign;
+			var sign = LC.CommonProperty.SIGN;
 			var progress = $("<div></div>").attr({
-				sign : signID,
+				sign : _signID,
 				"class" : "progress"
 			});
 			for (var i = 0; i < param[0]; i++) {
@@ -648,58 +698,127 @@ LC.Components.ProgressBar = function(progressID) {
 	/**
 	 * 获取进度条的DOM对像，通过.append()加入页面
 	 */
-	LC.Components.ProgressBar.prototype.progressDOM
-	if ( typeof this.listener != 'function') {
+	LC.Components.ProgressBar.prototype.progressDOM;
+	if ( typeof this.addListener != 'function') {
 		/**
-		 * 设置监听，在读条完成后调用函数
+		 * 设置监听，在读条完成后调用函数(一次只能设置一个)
 		 * @param {function} fn
+		 * @param {Object} num 子进度条顺位
 		 */
-		LC.Components.ProgressBar.prototype.listener = function(fn) {
+		LC.Components.ProgressBar.prototype.addListener = function(fn, num) {
+			num = LC.Components.ComponentFunction.checkNumInt(num, "设置进度条监听回调函数方法setlistener,num");
 			//获得组件内部div progressBar
 			var progressChilds = this.progressDOM.children("div.progress-bar");
-			progressChilds.first()[0].addEventListener('transitionend', fn, false);
+			progressChilds.eq(Number(num) - 1)[0].addEventListener('transitionend', fn, false);
+			return this;
+		};
+	};
+	if ( typeof this.removeListener != 'function') {
+		/**
+		 * 移除监听，移除在读条完成后调用函数(一次只能设置一个)
+		 * @param {function} fn
+		 * @param {Object} num 子进度条顺位
+		 */
+		LC.Components.ProgressBar.prototype.removeListener = function(fn, num) {
+			num = LC.Components.ComponentFunction.checkNumInt(num, "设置进度条监听回调函数方法setlistener,num");
+			//获得组件内部div progressBar
+			var progressChilds = this.progressDOM.children("div.progress-bar");
+			progressChilds.eq(Number(num) - 1)[0].removeEventListener('transitionend', fn, false);
+			return this;
+		};
+	};
+	//修改过渡效果
+	var _transitionMap = new LC.Utils.Map();
+	if ( typeof this.setTransition != 'function') {
+		/**
+		 * 修改过渡效果(一次只能设置一个，除长度外，可以在尾部添加其他过渡效果)
+		 * @param {Object} str css的过渡效果，只能是String
+		 * @param {Object} num 子进度条顺位
+		 */
+		LC.Components.ProgressBar.prototype.setTransition = function(str, num) {
+			num = LC.Components.ComponentFunction.checkNumInt(num, "修改进度条过渡效果方法setTransition,num");
+			if ( typeof str == "string") {//如果不为string,则设置无效
+				_transitionMap.put(num.toString(), str);
+			} else {
+				LC.warning("修改过渡效果失败，setTransition方法接收了不为String的无效参数！");
+			};
+			return this;
+		};
+	};
+	//获取过渡效果Map
+	if ( typeof this.getTransition != 'function') {
+		/**
+		 * 获取进度条过渡效果Map
+		 * @return {Map} _transitionMap
+		 */
+		LC.Components.ProgressBar.prototype.getTransition = function() {
+			return _transitionMap;
+		};
+	};
+	//还原过渡效果
+	if ( typeof this.removeTransition != 'function') {
+		/**
+		 * 还原过渡效果(不传参则全清)
+		 * @param {Object} num num[] ...num
+		 */
+		LC.Components.ProgressBar.prototype.removeTransition = function(num) {
+			if (null == num) {
+				_transitionMap.clear();
+			} else {
+				var param = LC.Components.ComponentFunction.checkInt(arguments, "还原进度条过渡效果方法setTransition");
+				for (var i=0; i < param.length; i++) {
+					_transitionMap.remove(Number(param[i]).toString);
+				};
+				//num = LC.Components.ComponentFunction.checkNumInt(num, "还原进度条过渡效果方法setTransition,num");
+				//_transitionMap.remove(num.toString);
+			}
+			return this;
+		};
+	};
+	//设置颜色，立即生效
+	if ( typeof this.setColor != 'function'){
+		LC.Components.ProgressBar.prototype.setColor = function(color,num) {
+			num = LC.Components.ComponentFunction.checkNumInt(num, "修改进度条过渡效果方法setTransition,num");
+			if ( typeof color == "string") {//如果color不为string,则设置无效
+				//获得组件内部div progressBar
+				var progressChild = this.progressDOM.children("div.progress-bar").eq(Number(num)-1);
+				progressChild.css({
+					"background-color" : color
+				});
+			} else {
+				LC.warning("设置颜色失败，setColor方法接收了不为String的无效参数！");
+			};
 			return this;
 		};
 	};
 	if ( typeof this.start != 'function') {
 		/**
-		 * 运行，开始读条到指定百分比
-		 * param {Object} widthPercentage 百分比
+		 * 运行，开始读条到设定百分比
 		 */
-		LC.Components.ProgressBar.prototype.start = function(widthPercentage) {
+		LC.Components.ProgressBar.prototype.start = function() {
 			//获得组件内部div progressBar
 			var progressChilds = this.progressDOM.children("div.progress-bar");
+			var jQueryObject;
+			//子进度条DOM对象
 			//重新设置运行时间样式（安全设置，防止页面修改）
-			progressChilds.first().css({
-				"-webkit-transition" : " width " + runningTime + "s linear ",
-				"-moz-transition" : " width " + runningTime + "s linear ",
-				"-o-transition" : " width " + runningTime + "s linear ",
-				"transition" : " width " + runningTime + "s linear "
-			});
-			progressChilds.first().next().css({
-				"-webkit-transition" : " width " + runningTime + "s linear ",
-				"-moz-transition" : " width " + runningTime + "s linear ",
-				"-o-transition" : " width " + runningTime + "s linear ",
-				"transition" : " width " + runningTime + "s linear "
-			});
-			if (!(null == widthPercentage || "" == widthPercentage)) {
-				if ("%" == widthPercentage.substring(widthPercentage.length - 1)) {
-					widthPercentage = widthPercentage.substring(0, widthPercentage.length - 1);
+			for (var i = 0; i < progressChilds.length; i++) {
+				jQueryObject = progressChilds.eq(i);
+				var childTime = this.getRunningTime().get((i + 1).toString());
+				var childTransition = this.getTransition().get((i + 1).toString());
+				if (null == childTransition) {
+					childTransition = " linear ";
 				};
-				var patt1 = new RegExp(/^[0-1]?\d{1,2}(\.\d+)?$/);
-				if (patt1.test(widthPercentage)) {
-					this.setWidth(widthPercentage);
-				} else {
-					LC.warning("传入值必须为0-100或0%-100%的数字,可以是小数");
-				}
-			}
-			progressChilds.first().css({
-				"width" : widthPercentage + "%"
-			});
-			progressChilds.first().next().css({
-				"width" : 100 - widthPercentage + "%"
-			});
-			;
+				jQueryObject.css({
+					"-webkit-transition" : " width " + childTime + "s " + childTransition,
+					"-moz-transition" : " width " + childTime + "s " + childTransition,
+					"-o-transition" : " width " + childTime + "s " + childTransition,
+					"transition" : " width " + childTime + "s " + childTransition
+				});
+				var childWidth = this.getWidth().get((i + 1).toString());
+				jQueryObject.css({
+					"width" : childWidth + "%"
+				});
+			};
 			return this;
 		};
 	}
@@ -748,8 +867,8 @@ LC.Components.ProgressBarFactory = {
 	 * @param {Object} progressID 进度条id
 	 */
 	createProgressBar : function(progressID) {
-		var returnProgress = new LC.Components.ProgressBar(progressID);
-		returnProgress.creatProgressDOM();
+		var returnProgress = new LC.Components.ProgressBar();
+		returnProgress.setSignID(progressID).creatProgressDOM(2);
 		return returnProgress;
 	},
 	createProgressBarStyle1 : function(progressID) {
