e6eb278fb7743c30ddcbf49ccc6c33d91048e5ac
guygu7 <599266988@qq.com>
2018-07-02 23:35:34 +0800

    1

---
+++ new/js/viewData.js
@@ -0,0 +1,112 @@
+/*
+* angularjs数据绑定示例（指对象绑定，非基本数据绑定）
+* ps：	1.任何绑定，绑定之后都不可在变更基本绑定对象，包括重新定义和数据覆盖，只能变更对象属性，否则将无法自动刷新数据
+* 		2.属性可以任意变更
+*/
+/*
+绑定可以接受对象类型和数组类型
+定义
+var viewDataModel={"viewDataModelname":"viewDataModelnamedata"};
+或
+var viewDataModel={viewDataModelname:"viewDataModelnamedata"};
+绑定
+$rootScope.viewDataModel = viewDataModel;
+展示
+{{viewDataModel.viewDataModelname}}
+
+定义
+var viewDataModel=["viewDataModelnamedata"];
+绑定
+$rootScope.viewDataModel = viewDataModel;
+展示
+{{viewDataModel[0]}}
+*/
+
+/**
+ * 视图数据显示对象 
+ */
+var viewDataModel = {
+	/**
+	 * 交流信息面板文本
+	 */
+	panelText:"",
+	/**
+	 * XX面板文本 
+	 */
+	panel2Text:"中文",
+	/**
+	 * XX面板 
+	 */
+	panel3:{
+		/**
+		 * 标题文本 
+		 */
+		tittleText:"标题",
+	},
+	/**
+	 * 人物交互面板(按钮组)
+	 */
+	panel4:{
+		/**
+		 * 按钮元素集合
+		 */
+		buttonElements:{},
+	},
+	/**
+	 * 菜单
+	 */
+	menu:{
+		/**
+		 * 菜单标题文本 
+		 */
+		tittleText:"菜单标题",
+		/**
+		 * 按钮元素集合（文本）
+		 */
+		buttonElements:{},
+	},
+	/**
+	 * 仓库面板 
+	 */
+	panel5:{
+		/**
+		 * 按钮元素集合（文本）
+		 */
+		buttonElements:{},
+	},
+	
+};
+/**
+ * 视图控制对象（属性为对视图的控制属性）
+ */
+var viewControl = {
+	/**
+	 * 应用全局对显示的控制(初始值)
+	 */
+	display:{
+		/**
+		 * 开始界面场景隐藏
+		 */
+		sceneStartHide:false,
+		/**
+		 * xx场景隐藏
+		 */
+		sceneMainHide:true,
+		/**
+		 * 菜单隐藏 
+		 */
+		menuHide:true,
+		/**
+		 * 遮罩层隐藏 
+		 */
+		maskLayerHide:true,
+		/**
+		 * 仓库组件隐藏
+		 */
+		panel5Hide:true,
+	},
+	/**
+	 * 当前所在场景（初始值） 
+	 */
+	currentScene : "scene-start",
+};
